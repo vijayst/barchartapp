328d7b2495b38cf79d10e2f5246683a88c1c59b8
vijayst <vijayst@gmail.com>
2018-11-10 20:52:35 +0530

    basic bar chart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import data from './data';
+import { max } from 'd3-array';
+import { scaleLinear, scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
 
 class App extends Component {
   render() {
+    const colorScale = scaleOrdinal(schemeCategory10);
+    const widthScale = scaleLinear().domain([0, max(data, d => d.wages)]).range([0, 320]);
+
     return (
       <table>
         <thead>
@@ -13,7 +19,7 @@ class App extends Component {
           </tr>  
         </thead>
         <tbody>
-          {data.map(d => (
+          {data.map((d, i) => (
             <tr key={d.country}>
               <td>
                 {d.country}
@@ -22,7 +28,9 @@ class App extends Component {
                 {d.wages.toFixed(2)}
               </td>
               <td>
-                
+                <svg height="50">
+                  <rect width={widthScale(d.wages)} height="50" fill={colorScale(i)} />
+                </svg>
               </td>
             </tr>
           ))}
