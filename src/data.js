76e9e1737dfa065bad186c47bbcb5791c15a1457
vijayst <vijayst@gmail.com>
2018-11-10 18:12:32 +0530

    barchart data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        country: 'India',
+        wages: .31
+    },
+    {
+        country: 'China',
+        wages: .85
+    },
+    {
+        country: 'USA',
+        wages: 7.25
+    },
+    {
+        country: 'UK',
+        wages: 10.04
+    },
+    {
+        country: 'Australia',
+        wages: 14.56
+    },
+    {
+        country: 'Canada',
+        wages: 8.46
+    },
+    {
+        country: 'Germany',
+        wages: 9.97
+    },
+    {
+        country: 'Saudi Arabia',
+        wages: 3.85
+    },
+    {
+        country: 'Brazil',
+        wages: 1.7
+    },
+    {
+        country: 'Japan',
+        wages: 6.78
+    },
+];
+
+export default data;
