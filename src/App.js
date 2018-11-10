4f9c202760d9909fa93571e5150f44b988b9a034
vijayst <vijayst@gmail.com>
2018-11-10 18:44:09 +0530

    data in a neat table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import data from './data';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello world
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th width="20%">Country</th>
+            <th width="20%">$ /hr</th>
+            <th></th> 
+          </tr>  
+        </thead>
+        <tbody>
+          {data.map(d => (
+            <tr key={d.country}>
+              <td>
+                {d.country}
+              </td>
+              <td>
+                {d.wages.toFixed(2)}
+              </td>
+              <td>
+                
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
